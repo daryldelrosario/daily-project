d3538f9c2c3ed0e8cbf0c1c0d2cecf74716a1696
Daryl del Rosario <daryldelrosario.is@gmail.com>
2022-11-24 01:37:55 -0500

    Create copy function for current date

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
+let currentDate = new Date();
+let displayDate = currentDate.toString();
+const today = document.querySelector("#today");
+today.textContent = displayDate;
+
+let lastDate = "Thu Nov 24 2022 01:32:05 GMT-0500 (Eastern Standard Time)";
+const lastStreak = document.querySelector("#last-streak");
+lastStreak.textContent = lastDate;
+
 const date = document.querySelector("#theDate");
-const todayIs = getCurrentDate();
-
-function showCurrentDate() {
-    let currentDate = new Date();
-    let displayDate = currentDate.toString();
-    alert(displayDate);
-}
-
-function pasteThis() {
-    let pasteDate = "Thu Nov 24 2022 00:45:07 GMT-0500 (Eastern Standard Time)"
-    date.textContent = pasteDate;
+let copiedDate = document.querySelector("#today").textContent;
+function copyDate() {
+    try {
+        navigator.clipboard.writeText(copiedDate);
+        console.log("First One");
+        alert("Copied " + copiedDate);
+    } catch(err) {
+        console.log("Second One");
+        alert("Failed to copy: " , err);
+    }
 }
