15e924b4d04c87d9f125bcc6587a4d62a9955a84
Daryl del Rosario <daryldelrosario.is@gmail.com>
2022-11-28 06:07:51 -0500

    Update file

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let displayDate = currentDate.toString();
 const today = document.querySelector("#today");
 today.textContent = displayDate;
 
-let lastDate = "Sun Nov 27 2022 21:33:09 GMT-0500 (Eastern Standard Time)";
+let lastDate = "Mon Nov 28 2022 06:07:23 GMT-0500 (Eastern Standard Time)";
 const lastStreak = document.querySelector("#last-streak");
 lastStreak.textContent = lastDate;
 
