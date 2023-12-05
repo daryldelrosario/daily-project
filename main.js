94dee5a60523b498ac0b381bbd5db2f54e505eff
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-12-05 08:06:17 -0500

    update file

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let displayDate = currentDate.toString();
 const today = document.querySelector("#today");
 today.textContent = displayDate;
 
-let lastDate = "Sun Dec 03 2023 14:34:49 GMT-0500 (Eastern Standard Time)";
+let lastDate = "Tue Dec 05 2023 08:04:09 GMT-0500 (Eastern Standard Time)";
 const lastStreak = document.querySelector("#last-streak");
 lastStreak.textContent = lastDate;
 
